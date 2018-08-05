b38648b5906005e73836b10e529bb70c7777e876
mujian <mujian@panhaidata.com>
2018-08-05 17:43:11 +0800

    #ignore

---
+++ springboot-globalexception/src/main/java/cn/leon/exception/Result.java
@@ -0,0 +1,45 @@
+package cn.leon.exception;
+
+public class Result<T> {
+
+
+    public static final Integer OK = 0;
+    public static final Integer ERROR = 100;
+
+    private String code;
+    private String url;
+    private String msg;
+    private T data;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
