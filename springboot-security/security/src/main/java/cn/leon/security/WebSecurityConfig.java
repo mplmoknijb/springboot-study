f98e6ab83c9f1a8b1a292e7146fe8f314dea9a93
mujian <m_jevon@163.com>
2019-12-24 09:16:38 +0800

    feat: sso

--- springboot-security/security/src/main/java/cn/leon/security/WebSecurityConfig.java
+++ springboot-security/security/src/main/java/cn/leon/security/WebSecurityConfig.java
@@ -1,10 +1,19 @@
 package cn.leon.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Class NameWebSecurityConfig
@@ -32,8 +41,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = createDelegatingPasswordEncoder();
         auth
                 .inMemoryAuthentication()
-                .withUser("mj").password("{noop}mj").roles("USER");
+                .withUser("mj").password(passwordEncoder.encode("mj")).roles("USER");
+    }
+
+    private PasswordEncoder createDelegatingPasswordEncoder() {
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        return new DelegatingPasswordEncoder("bcrypt", encoders);
     }
 }
